e4810ea5aed97477ac56dc49595af0d3f246a636
patricksinclair <pcsinclair@outlook.com>
2025-12-19 16:17:39 +0000

    two oled working

---
+++ keyboards/sofle/rev1/keymaps/vial/keymap.c
@@ -0,0 +1,124 @@
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ * Modification for Vial support by Drew Petersen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// clang-format off
+
+#include QMK_KEYBOARD_H
+#include "oled.c"
+
+// Default keymap. This can be changed in Vial. Use oled.c to change beavior that Vial cannot change.
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  ESC |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Caps |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LAlt | LGUI | LCTR |LOWER | /Space  /       \Space \  |RAISE | RCTR | RGUI | RAlt |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[0] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,  KC_RBRC,
+  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_MPLY, KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LALT, KC_LGUI, KC_LCTL, MO(2), KC_SPC,      KC_SPC,  MO(3), KC_RCTL, KC_RGUI, KC_RALT
+),
+/*
+ * COLEMAK
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[1] = LAYOUT(
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
+  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LGUI,KC_LALT,KC_LCTL,KC_TRNS, KC_ENT,      KC_SPC,  KC_TRNS, KC_RCTL, KC_RALT, KC_RGUI
+),
+/* LOWER
+
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | GRV  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TRNS | F12  | UP   | TRNS | TRNS | TRNS |                    | TRNS | TRNS | TRNS | PSCR | SCRL | PAUS |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | TRNS | LEFT | DOWN | RGHT | TRNS | TRNS |-------.    ,-------| TRNS | TRNS | '/@ | INS  | HOME | PGUP |
+ * |------+------+------+------+------+------| MUTE  |    | Play  |------+------+------+------+------+------|
+ * | TRNS | LCTL+Z | LCTL+X | LCTL+C | LCTL+V|-------|    |-------| TRNS | TRNS | TRNS | DEL  | END  | PGDN |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | TRNS | TRNS | TRNS | TRNS | / Space /       \Space \  | TRNS | TRNS | TRNS | TRNS |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[2] = LAYOUT(
+  KC_GRV,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  _______,  KC_F12,     KC_UP,      _______,    _______,    _______,                            _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS,
+  _______,  KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______,                            _______, _______, KC_QUOT, KC_INS,  KC_HOME, KC_PGUP,
+  _______,  LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, _______,         _______,  _______, _______, _______, KC_DEL,  KC_END,  KC_PGDN,
+                       _______, _______, _______, _______, _______,                         _______,    _______,  _______, _______, _______
+),
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | GRV  |      |      |      |      |      |                    |      |      |      |  -   |  =   | DEL  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |  UP  |      |      |      |                    |      |      |      |      |  [   |  ]   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | LEFT | DOWN | RGHT |      |      |-------.    ,-------|      |      |  '/@ | #/~  |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      | \/| | CUT  | COPY | PASTE |      |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            |      |      |      |      | /       /       \      \ |      |      |      |      |
+ *            `----------------------------------'         '------''---------------------------'
+ */
+
+[3] = LAYOUT(
+  KC_GRV,  _______, _______, _______,  _______,  _______,                           _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
+  _______, _______, KC_UP,   _______,  _______,  _______,                           _______, _______, _______, _______, KC_LBRC, KC_RBRC,
+  _______, KC_LEFT, KC_DOWN, KC_RGHT,  _______,  _______,                           _______, _______, KC_QUOT, KC_NUHS, _______, _______,
+  _______, KC_NUBS, KC_CUT,  KC_COPY,  KC_PASTE, _______,  _______,       _______,  _______, _______, _______, _______, _______, _______,
+                    _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______
+)
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+};
